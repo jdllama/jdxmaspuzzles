07bdd84cfb275a6672621e7dd126b757e2b0541e
J.D. Lowe <jdlowe.dev@gmail.com>
2017-12-06 22:09:51 -0500

    Working on building more Express

--- index.js
+++ index.js
@@ -6,4 +6,14 @@ express()
     .use(express.static(path.join(__dirname, 'public')))
     .set("views", path.join(__dirname, 'views'))
     .set("view engine", "ejs")
+    .use(bodyParser.urlencoded({ extended: false }))
+    .use(bodyParser.json())
+    .use(cookieParser())
+    .use(function(req, res, next) {
+  for (var key in req.query)
+  { 
+    req.query[key.toLowerCase()] = req.query[key];
+  }
+  next();
+})
     .listen(PORT, () => console.log(`Listening on ${ PORT }`));
