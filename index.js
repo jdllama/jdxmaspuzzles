224b9dcaeddee16fb47b88714e27f5a9ecfd4f39
J.D. Lowe <jdlowe.dev@gmail.com>
2017-12-07 19:44:39 -0500

    ...Maybe?

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 5000;
 const cookieParser = require("cookie-parser");
 const bodyParser = require('body-parser');
 
-let app = express()
+var app = express()
     .use(express.static(path.join(__dirname, 'public')))
     .set("views", path.join(__dirname, 'views'))
     .set("view engine", "ejs")
