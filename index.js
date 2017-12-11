d4af739738719c46986502eaacfa6345137713d4
J.D. Lowe <jdlowe.dev@gmail.com>
2017-12-10 21:38:56 -0500

    Dammit

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')))
     connection.query('SELECT (SELECT COUNT(*) from puzzles) as total, (SELECT COUNT(*) from puzzles where issolved = 0) as remaining, (SELECT COUNT(*) from metas) as totalMeta, (SELECT COUNT(*) from metas where issolved = 0) as remainingMeta, (SELECT COUNT(*) from final) as totalFinal, (SELECT COUNT(*) from final where issolved = 0) as remainingFinal;', function(err, rows, fields) {
       if (err) throw err;
       //console.dir(rows)
-      res.render("pages/index", {message: "", username: req.cookies.username, total: rows[0].total, remaining: rows[0].remaining, totalMeta: rows[0].totalMeta, remainingMeta: rows[0].remainingMeta});
+      res.render("pages/index", {message: "", username: req.cookies.username, total: rows[0].total, remaining: rows[0].remaining, totalMeta: rows[0].totalMeta, remainingMeta: rows[0].remainingMeta, totalFinal: rows[0].totalFinal, remainingFinal: rows[0].remainingFinal});
     });
     connection.end();
   });
