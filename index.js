95b60ba3c38e022a7a87b6e2ba3457f1bc8cc829
J.D. Lowe <jdlowe.dev@gmail.com>
2017-12-19 22:19:29 -0500

    Progress!

--- index.js
+++ index.js
@@ -37,11 +37,19 @@ app.use(express.static(path.join(__dirname, 'public')))
     var connection = mysql.createConnection(process.env.JAWSDB_URL);
 
     connection.connect();
-    connection.query('SELECT (SELECT name from puzzles where issolved = 0) as allGuessed, (SELECT COUNT(*) from puzzles) as total, (SELECT COUNT(*) from puzzles where issolved = 0) as remaining, (SELECT COUNT(*) from metas) as totalMeta, (SELECT COUNT(*) from metas where issolved = 0) as remainingMeta, (SELECT COUNT(*) from final) as totalFinal, (SELECT COUNT(*) from final where issolved = 0) as remainingFinal;', function(err, rows, fields) {
-      if (err) throw err;
-      //console.dir(rows)
-      res.render("pages/index", {message: "", username: req.cookies.username, allGuessed: rows[0].allGuessed, total: rows[0].total, remaining: rows[0].remaining, totalMeta: rows[0].totalMeta, remainingMeta: rows[0].remainingMeta, totalFinal: rows[0].totalFinal, remainingFinal: rows[0].remainingFinal});
+    connection.query('SELECT name from puzzles where issolved = 0', function(err, topRows, fields) {
+      var allGuessed = topRows;
+      var connection = mysql.createConnection(process.env.JAWSDB_URL);
+
+      connection.connect();
+      connection.query('SELECT (SELECT COUNT(*) from puzzles) as total, (SELECT COUNT(*) from puzzles where issolved = 0) as remaining, (SELECT COUNT(*) from metas) as totalMeta, (SELECT COUNT(*) from metas where issolved = 0) as remainingMeta, (SELECT COUNT(*) from final) as totalFinal, (SELECT COUNT(*) from final where issolved = 0) as remainingFinal;', function(err, rows, fields) {
+        if (err) throw err;
+        //console.dir(rows)
+        res.render("pages/index", {message: "", username: req.cookies.username, allGuessed: topRows, total: rows[0].total, remaining: rows[0].remaining, totalMeta: rows[0].totalMeta, remainingMeta: rows[0].remainingMeta, totalFinal: rows[0].totalFinal, remainingFinal: rows[0].remainingFinal});
+      });
+      connection.end();
     });
+    
     connection.end();
   });
 
