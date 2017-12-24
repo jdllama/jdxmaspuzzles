769a49d724b003cd2c4c78d593d01c881d7626c1
J.D. Lowe <jdlowe.dev@gmail.com>
2017-12-23 23:26:05 -0500

    WHOO YEAH

--- public/script.js
+++ public/script.js
@@ -1,20 +1,13 @@
 window.getCookie = function(name) {
     match = document.cookie.match(new RegExp(name + '=([^;]+)'));
     if (match) return match[1];
-  }
-  
+}
 
 window.onload = function() {
-    /*
-    document.getElementById("myForm").onsubmit = function() {
-
-        alert(getCookie("username");
-        return true;
-    }
-    */
-    console.log("Yes?");
     document.getElementById("myForm").addEventListener("submit", function(event) {
-        event.preventDefault();
-        alert(getCookie("username"));
+        if(typeof getCookie("username") == "undefined") {
+            event.preventDefault();
+            alert("Please set a username and try the answer again.");
+        }
     });
 }
