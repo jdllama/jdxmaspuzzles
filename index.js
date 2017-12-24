315baf1644645c113c60c4ee3debe3f001d3536a
J.D. Lowe <jdlowe.dev@gmail.com>
2017-12-23 22:11:54 -0500

    Not there yet but close

--- index.js
+++ index.js
@@ -101,7 +101,28 @@ app.use(express.static(path.join(__dirname, 'public')))
         var connection = mysql.createConnection(process.env.JAWSDB_URL);
         connection.connect();
         connection.query('SELECT  * FROM guesses where puzzlename = ? ORDER BY `timestamp` DESC', [name], function(err, rowsTop, fields) {
-          res.render("pages/puzzle", {partial: partial, name: name, title: title, message: "", username: username, guesses: rowsTop, isRight: isRight});
+          var connection = mysql.createConnection(process.env.JAWSDB_URL);
+
+          connection.connect();
+          connection.query('SELECT (SELECT COUNT(*) from puzzles) as total, (SELECT COUNT(*) from puzzles where issolved = 0) as remaining, (SELECT COUNT(*) from metas) as totalMeta, (SELECT COUNT(*) from metas where issolved = 0) as remainingMeta, (SELECT COUNT(*) from final) as totalFinal, (SELECT COUNT(*) from final where issolved = 0) as remainingFinal;', function(err, rows, fields) {
+            if (err) throw err;
+            res.render("pages/puzzle", {
+              partial: partial, 
+              name: name, 
+              title: title, 
+              message: "", 
+              username: username, 
+              guesses: rowsTop, 
+              isRight: isRight,
+              total: rows[0].total, 
+              remaining: rows[0].remaining, 
+              totalMeta: rows[0].totalMeta, 
+              remainingMeta: rows[0].remainingMeta, 
+              totalFinal: rows[0].totalFinal, 
+              remainingFinal: rows[0].remainingFinal
+            });
+          });
+          connection.end();
         });
         connection.end();
       });
